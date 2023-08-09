68b7ebfe84ead915eacac278b471819f66beb5da
jia <jia@gmail.com>
2023-08-09 20:19:18 +0000

    register

---
+++ src/main/java/authenticationService/RegisterApi.java
@@ -0,0 +1,92 @@
+package authenticationService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@CrossOrigin
+@RequestMapping("/account")
+@RestController
+public class RegisterApi {
+
+	
+	@GetMapping("/register")
+	public String getString() {
+		return "GET for token";
+	}
+	
+//	@PostMapping("/register")
+//	public ResponseEntity<?> registerCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri) {
+//		System.out.println(newCustomer.toString());
+//		System.out.println(uri.toString());
+//		if (newCustomer.getEmail() == null) {
+//			return ResponseEntity.badRequest().build();
+//		}	
+//		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+//				.buildAndExpand(newCustomer.getId()).toUri();
+//		ResponseEntity<?> response = ResponseEntity.created(location).build();
+//		System.out.println(response.toString());
+//		return response;
+//	}
+
+	@PostMapping("/register")
+	public void postNewCustomerToCustomerAPI(@RequestBody String json_string) {
+		System.out.println("postNewCustomerToCustomerAPI");
+		System.out.println(json_string);
+		try {
+
+			URL url = new URL("http://localhost:8080/api/customers");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+	  		//Token token = TokenAPI.getAppUserToken();
+	  		//conn.setRequestProperty("authorization", "Bearer " + token.getToken());
+	  		// conn.setRequestProperty("tokencheck", "false");
+
+			OutputStream os = conn.getOutputStream();
+			os.write(json_string.getBytes());
+			os.flush();
+
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+}
